d6b25ae4ea63b0081cad726bbf44c1ad50316f37
Prakash Saini <35262074+Prakash-sa@users.noreply.github.com>
2021-01-30 18:11:38 +0530

    Update therapist.py

--- therapist.py
+++ therapist.py
@@ -6,7 +6,7 @@ import os # accessing directory structure
 import pandas as pd 
 
 nRowsRead = 1000 
-df1 = pd.read_csv('/kaggle/input/Psych_data.csv', delimiter=',', nrows = nRowsRead)
+df1 = pd.read_csv('Psych_data.csv', delimiter=',', nrows = nRowsRead)
 df1.dataframeName = 'Psych_data.csv'
 nRow, nCol = df1.shape
 print(f'There are {nRow} rows and {nCol} columns')
