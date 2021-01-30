b00eb1d3e260ea557373d4be93597de097c44fb6
Prakash Saini <35262074+Prakash-sa@users.noreply.github.com>
2021-01-30 18:10:39 +0530

    machine_learning

---
+++ therapist.py
@@ -0,0 +1,21 @@
+from mpl_toolkits.mplot3d import Axes3D
+from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt 
+import numpy as np 
+import os # accessing directory structure
+import pandas as pd 
+
+nRowsRead = 1000 
+df1 = pd.read_csv('/kaggle/input/Psych_data.csv', delimiter=',', nrows = nRowsRead)
+df1.dataframeName = 'Psych_data.csv'
+nRow, nCol = df1.shape
+print(f'There are {nRow} rows and {nCol} columns')
+
+df1.head(5)
+
+
+
+plotPerColumnDistribution(df1, 10, 5)
+
+
+plotCorrelationMatrix(df1, 8)
